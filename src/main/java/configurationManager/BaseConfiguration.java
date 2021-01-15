344d5003ea019aed001c740bf712e880ceea632d
svetlanamikolaenko <svetlana.mikolaenko@gmail.com>
2021-01-15 13:33:12 +0200

    addArgument for windows

--- src/main/java/configurationManager/BaseConfiguration.java
+++ src/main/java/configurationManager/BaseConfiguration.java
@@ -14,7 +14,7 @@ public class BaseConfiguration {
             return driver;
         }
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-data-dir=C:/Users/User/AppData/Local/Google/Chrome/User");
+        options.addArguments("user-data-dir=C:\\Users\\Svitlana Mykolaienko\\AppData\\Local\\Google\\Chrome\\User Data");
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
